4e617f564ac2fa74b3ce655649c8d2a9ff8c191f
Michael W <w.eso@live.de>
2022-12-15 21:23:01 +0100

    test rc2

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
 console.log('background worker started')
-var focus_mate_tabs = []
+chrome.tabs.onRemoved.addListener(close_check)
 
 
 try{
     chrome.runtime.onMessage.addListener(async function(request, sender) {
-		console.log("sender.tab.id")
-		console.log(sender.tab.id)
+		//console.log("sender.tab.id")
+		//console.log(sender.tab.id)
         if (request.type == "muteType"){
-            console.log("recieved muteType")
-            console.log(request.status)
-			focus_mate_tabs.push(sender.tab.id)
+            //console.log("recieved muteType")
+            //console.log(request.status)
 			if (request.status == "false"){
 				turn_Off_Sound()
 			}else{
@@ -24,7 +23,6 @@ function returnMessage(messageToReturn)
 {
  chrome.tabs.getSelected(null, function(tab) {
   var joinedMessage = messageToReturn + backgroundScriptMessage;  
-  alert("Background script is sending a message to contentscript:'" + joinedMessage +"'");
   chrome.tabs.sendMessage(tab.id, {message: joinedMessage});
  });
 }
@@ -33,10 +31,12 @@ function turn_Off_Sound() {
   chrome.tabs.query({url: []}, function (tabs) {
     for (var i = 0; i < tabs.length; i++) {
       var mutedInfo = tabs[i].mutedInfo;
-	  if (tabs[i].id in focus_mate_tabs){}
-	  else{
-		if (mutedInfo) chrome.tabs.update(tabs[i].id, {"muted": true});
-	  }
+		if (mutedInfo){
+			//console.log('turn_off',tabs[i].url)
+			if (!tabs[i].url.includes("focusmate.com")){
+				chrome.tabs.update(tabs[i].id, {"muted": true});
+			}
+		}
     }
 })}
 
@@ -44,6 +44,20 @@ function turn_On_Sound() {
   chrome.tabs.query({url: []}, function (tabs) {
     for (var i = 0; i < tabs.length; i++) {
       var mutedInfo = tabs[i].mutedInfo;
-      if (mutedInfo) chrome.tabs.update(tabs[i].id, {"muted": false});
+      if (mutedInfo){
+		//console.log('turn_on',tabs[i].url)
+		if (!tabs[i].url.includes("focusmate.com")){
+				chrome.tabs.update(tabs[i].id, {"muted": false});
+		}
+	  }
     }
 })}
+
+function close_check(tabid, removed) {
+	console.log(tabid)
+	console.log('above tab was closed')
+	
+	chrome.tabs.query({url: []}, function (tabs) {
+		
+	}
+}
